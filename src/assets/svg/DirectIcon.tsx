92a65d8f18a7d6836c2c840d1610eca1f2ae444c
yoosion030 <rsy5487@naver.com>
2022-12-19 20:14:28 +0900

    Add Icon

---
+++ src/assets/svg/DirectIcon.tsx
@@ -0,0 +1,16 @@
+const DirectIcon = () => (
+  <svg
+    width="203"
+    height="150"
+    viewBox="0 0 203 150"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M90 150H-22.5V125H90V150ZM202.5 87.5H-22.5V62.5H202.5V87.5ZM202.5 25H90V0H202.5V25Z"
+      fill="#F98543"
+    />
+  </svg>
+);
+
+export default DirectIcon;
--- src/assets/svg/index.ts
+++ src/assets/svg/index.ts
@@ -1,1 +1,4 @@
 export { default as Logo } from './Logo';
+export { default as BoardIcon } from './BoardIcon';
+export { default as DirectIcon } from './DirectIcon';
+export { default as RandomIcon } from './RandomIcon';
