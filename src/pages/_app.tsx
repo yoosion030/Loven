4b8418800ad53bd1b364083bde4eaeba0474ebbc
yoosion030 <rsy5487@naver.com>
2022-12-19 14:37:59 +0900

    Update font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,20 @@
 import type { AppProps } from 'next/app';
 import { GlobalStyle } from 'shared/styles/GlobalStyle';
+import { Archivo_Black, Noto_Sans } from '@next/font/google';
+
+export const archive_black = Archivo_Black({
+  weight: ['400'],
+  variable: '--font-archivo_black',
+  subsets: ['latin'],
+});
+export const noto_sans = Noto_Sans({ weight: ['400'] });
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <main>
       <GlobalStyle />
       <Component {...pageProps} />
-    </>
+    </main>
   );
 }
 
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -16,10 +16,6 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link
-            href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;600;700;900&display=swap"
-            rel="stylesheet"
-          />
           <link rel="icon" href="/favicon.png" />
         </Head>
         <body>
