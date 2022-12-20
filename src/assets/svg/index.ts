eced574f5cc89c6799ef4e86dd3a708a3a9cdfd7
yoosion030 <rsy5487@naver.com>
2022-12-21 08:55:03 +0900

    Add PreviewIcon svg

--- src/assets/svg/index.ts
+++ src/assets/svg/index.ts
@@ -3,3 +3,4 @@ export { default as BoardIcon } from './BoardIcon';
 export { default as DirectIcon } from './DirectIcon';
 export { default as RandomIcon } from './RandomIcon';
 export { default as RandomBackground } from './RandomBackground';
+export { default as PreviewIcon } from './PreviewIcon';
