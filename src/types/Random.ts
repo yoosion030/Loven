5fd221a8e6b7a916a4cdcdd6ea1e72d5ffb6ca4d
yoosion030 <rsy5487@naver.com>
2022-12-20 16:14:20 +0900

    Add winner_kind

--- src/types/Random.ts
+++ src/types/Random.ts
@@ -1,5 +1,6 @@
 export interface RandomType {
   conferrer: string; // 수여자
-  winner: string; // 상여자
+  winner: string; // 수상자
   conferrer_group: string; // 수여자 소속
+  winner_kind: string; // 수상자 종류
 }
