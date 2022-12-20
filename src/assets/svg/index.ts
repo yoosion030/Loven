cb681e77527361f97961a774a1029597779b8478
yoosion030 <rsy5487@naver.com>
2022-12-20 10:50:25 +0900

    Add RandomBackground

--- src/assets/svg/index.ts
+++ src/assets/svg/index.ts
@@ -2,3 +2,4 @@ export { default as Logo } from './Logo';
 export { default as BoardIcon } from './BoardIcon';
 export { default as DirectIcon } from './DirectIcon';
 export { default as RandomIcon } from './RandomIcon';
+export { default as RandomBackground } from './RandomBackground';
