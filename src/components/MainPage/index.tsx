e70bc86f490c85cfd9565f5158dd11125ab18506
yoosion030 <rsy5487@naver.com>
2022-12-20 09:24:10 +0900

    Update Main Components

--- src/components/MainPage/index.tsx
+++ src/components/MainPage/index.tsx
@@ -1,54 +1,15 @@
-import * as I from 'assets/svg';
+import { Logo, BoardCell, DirectCell, RandomCell, TitleCell } from 'components';
 import * as S from './style';
 
 const MainPage = () => {
   return (
     <S.Container>
-      <S.LogoWrapper>
-        <I.Logo />
-      </S.LogoWrapper>
+      <Logo />
       <S.Grid>
-        <S.TitleBox>
-          <S.Title>
-            It is innovation <br />
-            that can be <br />
-            customized for listing.
-          </S.Title>
-          <S.Desc>세상에 없던 특별한 상장을 만듭니다.</S.Desc>
-        </S.TitleBox>
-        <S.RandomBox>
-          <div>
-            <S.BoxTitle>Random</S.BoxTitle>
-            <S.Desc>내 친구에겐 어떤 상이 어울릴까?</S.Desc>
-          </div>
-          <div>
-            <S.Count>24</S.Count>
-            <S.Participant>명의 참가자</S.Participant>
-          </div>
-          <I.RandomIcon />
-        </S.RandomBox>
-
-        <S.DirectBox>
-          <div>
-            <S.BoxTitle>Direct</S.BoxTitle>
-            <S.Desc>친구에게 직접 주고싶은 상이 있다면.</S.Desc>
-          </div>
-          <div>
-            <S.Count>17</S.Count>
-            <S.Participant>명의 참가자</S.Participant>
-          </div>
-          <I.DirectIcon />
-        </S.DirectBox>
-
-        <S.BoardBox>
-          <div>
-            <S.BoardTitle>
-              Bulletin <br /> board
-            </S.BoardTitle>
-            <S.Desc>상장, 모두가 받을 수 있도록.</S.Desc>
-          </div>
-          <I.BoardIcon />
-        </S.BoardBox>
+        <TitleCell />
+        <RandomCell />
+        <DirectCell />
+        <BoardCell />
       </S.Grid>
     </S.Container>
   );
--- src/components/MainPage/style.ts
+++ src/components/MainPage/style.ts
@@ -7,11 +7,6 @@ export const Container = styled.div`
   height: 100vh;
 `;
 
-export const LogoWrapper = styled.div`
-  text-align: center;
-  margin-bottom: 2rem;
-`;
-
 export const Grid = styled.div`
   flex: 1;
   display: grid;
@@ -23,105 +18,5 @@ export const Grid = styled.div`
   & div {
     background-color: #ffffff;
     border-radius: 1.25rem;
-  }
-`;
-
-export const TitleBox = styled.div`
-  grid-column: 1 / span 3;
-  padding: 6.25rem 5rem;
-`;
-
-export const Title = styled.h1`
-  font-weight: 400;
-  font-size: 4.375rem;
-  line-height: 5rem;
-`;
-
-export const BoxTitle = styled.h1`
-  font-weight: 400;
-  font-size: 3.125rem;
-  line-height: 3.375rem;
-`;
-
-export const BoardTitle = styled.h1`
-  font-size: 5rem;
-`;
-
-export const Desc = styled.p`
-  font-family: 'Noto Sans KR';
-  font-weight: 400;
-  font-size: 1.25rem;
-  line-height: 1.75rem;
-  color: #808080;
-`;
-
-export const Box = styled.div`
-  cursor: pointer;
-  position: relative;
-  gap: 1.875rem;
-  padding: 4.688em 5.313em;
-  &:hover {
-    color: #ffffff;
-    background-color: #000000;
-    div {
-      color: #ffffff;
-      background-color: #000000;
-    }
-  }
-`;
-
-export const Count = styled.h2`
-  margin-top: 1.875rem;
-  text-align: right;
-  font-size: 5.625rem;
-`;
-
-export const Participant = styled.p`
-  width: 7.5rem;
-  font-size: 0.75rem;
-  text-align: center;
-  margin-left: auto;
-`;
-
-export const RandomBox = styled(Box)`
-  svg {
-    position: absolute;
-    left: 1.25rem;
-    bottom: 0;
-  }
-`;
-
-export const DirectBox = styled(Box)`
-  svg {
-    position: absolute;
-    left: 0;
-    bottom: 1.25rem;
-  }
-`;
-
-export const BoardBox = styled.div`
-  cursor: pointer;
-  position: relative;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-
-  svg {
-    position: absolute;
-    left: 40%;
-    top: 33%;
-  }
-
-  &:hover {
-    color: #ffffff;
-    background-color: #000000;
-    div {
-      color: #ffffff;
-      background-color: #000000;
-    }
-    svg > path {
-      fill: #ffffff;
-    }
   }
 `;
