54a64e3e284ed7abec893f36582ebeece73cf363
yoosion030 <rsy5487@naver.com>
2022-12-22 15:50:16 +0900

    Add PrizeType

---
+++ src/types/Prize.ts
@@ -0,0 +1,8 @@
+export interface PrizeType {
+  id: number;
+  conferrer: string; // 수여자
+  winner: string; // 상여자
+  prize: string; // 상 이름
+  conferrer_group: string; // 수여자 소속
+  content: string; // 내용
+}
