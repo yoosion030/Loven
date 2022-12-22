eb5477d926a675316e7338bc0d8857f8dfa11cb9
yoosion030 <rsy5487@naver.com>
2022-12-22 20:58:53 +0900

    Add direct/board

--- src/pages/board/index.tsx
+++ src/pages/board/index.tsx
@@ -1,9 +1,9 @@
+import axios from 'axios';
 import { BoardPage } from 'components';
 import { GetServerSideProps } from 'next';
 import { PrizesType, PrizeType } from 'types/Prize';
 
 const index = (prizes: PrizesType) => {
-  console.log(prizes);
   return <BoardPage prizes={prizes} />;
 };
 
@@ -20,113 +20,9 @@ export const getServerSideProps: GetServerSideProps = async () => {
     },
   ];
 
-  const directPrizes: PrizeType[] = [
-    {
-      id: 1,
-      conferrer: '양세련',
-      winner: '김경남',
-      prize: '츤데레상츤데레상츤데레상',
-      conferrer_group: '광주소프트웨어마이스터고등학교',
-      content:
-        '위 사람은 평소 겉은 차가워 보이지만위 사람은 평소 겉은 차가워 보이지만위 사람은 평소 겉은 차가워 보이지만위 사람은 평소 겉은 차가워 보이지만',
-    },
-    {
-      id: 2,
-      conferrer: '양세련',
-      winner: '양세련',
-      prize: '명상',
-      content: '수업시간에 그만 좀 ',
-      conferrer_group: '광주소프트웨어마이스터고등학교',
-    },
-    {
-      id: 2,
-      conferrer: '양세련',
-      winner: '양세련',
-      prize: '명상',
-      content: '수업시간에 그만 좀 ',
-      conferrer_group: '광주소프트웨어마이스터고등학교',
-    },
-    {
-      id: 2,
-      conferrer: '양세련',
-      winner: '양세련',
-      prize: '명상',
-      content: '수업시간에 그만 좀 ',
-      conferrer_group: '광주소프트웨어마이스터고등학교',
-    },
-    {
-      id: 2,
-      conferrer: '양세련',
-      winner: '양세련',
-      prize: '명상',
-      content: '수업시간에 그만 좀 ',
-      conferrer_group: '광주소프트웨어마이스터고등학교',
-    },
-    {
-      id: 2,
-      conferrer: '양세련',
-      winner: '양세련',
-      prize: '명상',
-      content: '수업시간에 그만 좀 ',
-      conferrer_group: '광주소프트웨어마이스터고등학교',
-    },
-    {
-      id: 2,
-      conferrer: '양세련',
-      winner: '양세련',
-      prize: '명상',
-      content: '수업시간에 그만 좀 ',
-      conferrer_group: '광주소프트웨어마이스터고등학교',
-    },
-    {
-      id: 2,
-      conferrer: '양세련',
-      winner: '양세련',
-      prize: '명상',
-      content: '수업시간에 그만 좀 ',
-      conferrer_group: '광주소프트웨어마이스터고등학교',
-    },
-    {
-      id: 2,
-      conferrer: '양세련',
-      winner: '양세련',
-      prize: '명상',
-      content: '수업시간에 그만 좀 ',
-      conferrer_group: '광주소프트웨어마이스터고등학교',
-    },
-    {
-      id: 2,
-      conferrer: '양세련',
-      winner: '양세련',
-      prize: '명상',
-      content: '수업시간에 그만 좀 ',
-      conferrer_group: '광주소프트웨어마이스터고등학교',
-    },
-    {
-      id: 2,
-      conferrer: '양세련',
-      winner: '양세련',
-      prize: '명상',
-      content: '수업시간에 그만 좀 ',
-      conferrer_group: '광주소프트웨어마이스터고등학교',
-    },
-    {
-      id: 2,
-      conferrer: '양세련',
-      winner: '양세련',
-      prize: '명상',
-      content: '수업시간에 그만 좀 ',
-      conferrer_group: '광주소프트웨어마이스터고등학교',
-    },
-    {
-      id: 2,
-      conferrer: '양세련',
-      winner: '양세련',
-      prize: '명상',
-      content: '수업시간에 그만 좀 ',
-      conferrer_group: '광주소프트웨어마이스터고등학교',
-    },
-  ];
+  const directPrizes: PrizeType[] = (
+    await axios.get('http://10.82.17.155:8000/direct/board')
+  ).data;
 
   return {
     props: {
