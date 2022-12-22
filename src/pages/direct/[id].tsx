f28343d1b8d91be7352c64bd8973921cd136d783
yoosion030 <rsy5487@naver.com>
2022-12-22 20:58:39 +0900

    Add direct/id

--- src/pages/direct/[id].tsx
+++ src/pages/direct/[id].tsx
@@ -1,23 +1,20 @@
+import axios from 'axios';
 import { PrizePage } from 'components';
-import { GetServerSideProps, NextPage } from 'next';
+import { GetServerSideProps, GetServerSidePropsContext, NextPage } from 'next';
 import { PrizeProps, PrizeType } from 'types/Prize';
 
 const index: NextPage<PrizeProps> = ({ prize }) => {
   return <PrizePage prize={prize} />;
 };
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getServerSideProps: GetServerSideProps = async (
+  ctx: GetServerSidePropsContext,
+) => {
   console.log('직접 입력 상장 조회 api 요청');
 
-  const prize: PrizeType = {
-    id: 1,
-    conferrer: '양세련',
-    winner: '김경남',
-    prize: '츤데레상',
-    conferrer_group: '광주소프트웨어마이스터고등학교',
-    content:
-      '위 사람은 평소 겉은 차가워 보이지만위 사람은 평소 겉은 차가워 보이지만위 사람은 평소 겉은 차가워 보이지만위 사람은 평소 겉은 차가워 보이지만',
-  };
+  const prize: PrizeType = (
+    await axios.get(`http://10.82.17.155:8000/direct/detail/${ctx.params?.id}`)
+  ).data;
 
   return {
     props: {
