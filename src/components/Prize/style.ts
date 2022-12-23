66793ee8480a86d6c6620b26c402cf79e74c3178
yoosion030 <rsy5487@naver.com>
2022-12-23 09:20:54 +0900

    Update style

--- src/components/Prize/style.ts
+++ src/components/Prize/style.ts
@@ -15,7 +15,7 @@ export const Prize = styled.div`
 export const Title = styled.h1`
   text-align: center;
   font-size: 5.625rem;
-  margin: 3rem 6.25rem;
+  margin: 3rem 6.25rem 7rem;
   p {
     display: inline-block;
     margin-right: 1.875rem;
@@ -26,11 +26,7 @@ export const WinnerSection = styled.div`
   display: flex;
   font-size: 2.5rem;
   justify-content: space-between;
-  height: 6.25rem;
   padding: 0 8.75rem;
-  p {
-    width: 40%;
-  }
 
   .winner {
     width: 30%;
@@ -43,7 +39,7 @@ export const ContentSection = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  height: 34.375rem;
+  height: 34rem;
   line-height: 4.063rem;
   padding: 0 7.5rem;
   white-space: pre-line;
