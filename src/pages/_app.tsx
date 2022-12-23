54acb26e8d627fc0ef0a23351f6b4c88d05d96ed
yoosion030 <rsy5487@naver.com>
2022-12-23 14:05:58 +0900

    Update favicon, title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,14 @@
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 import { GlobalStyle } from 'shared/styles/GlobalStyle';
 import { Logo, Layout } from 'components';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <main>
+      <Head>
+        <title>Loven</title>
+      </Head>
       <GlobalStyle />
       <Logo />
       <Layout>
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -16,7 +16,7 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link rel="icon" href="/favicon.png" />
+          <link rel="icon" href="/favicon.ico" />
           <link rel="preload" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
